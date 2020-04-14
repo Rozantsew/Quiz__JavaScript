addb3daf8e7d3242cbd32755005ac235d424c282
Michal <rozancew@interia.pl>
2020-04-14 23:29:21 +0200

    html template created

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
-const article = document.querySelector('article');
+const correctAnswers = ['A', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
 
-console.log(article.children);
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    //check answers
+    userAnswers.forEach((answer, index) =>{
+        if (answer === correctAnswers[index]){
+            score += 25;
+        }
+    });
+
+    console.log(score);
+});
