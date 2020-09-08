9360a564e7c005156b166e700aa624a5d22f3126
Michal <rozancew@interia.pl>
2020-09-08 12:12:18 +0200

    Updates

---
+++ precache-manifest.44dea6fa655538e1445ed22646d4bb17.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afc8759a48a659990bfc846133c22802",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "34be509a2421a058bbe8",
+    "url": "/Portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e59684ce986c2fb14b06",
+    "url": "/Portfolio/static/js/2.ee9999c9.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/Portfolio/static/js/2.ee9999c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34be509a2421a058bbe8",
+    "url": "/Portfolio/static/js/main.de26f7ea.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "4a7ed733acb33be0eb2ab8526a8f4edb",
+    "url": "/Portfolio/static/media/Design_&_Codding.4a7ed733.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/Portfolio/static/media/Git-hub.0ff1a6f0.svg"
+  },
+  {
+    "revision": "057b0c3027668bb34adbfbdd0b73ca0a",
+    "url": "/Portfolio/static/media/Michael.057b0c30.png"
+  },
+  {
+    "revision": "1e7b01a1e8e8a086bdfa7f21decd01cc",
+    "url": "/Portfolio/static/media/Personal_Details.1e7b01a1.svg"
+  },
+  {
+    "revision": "09f577dd3ff900ba1bcd7a6fe2553540",
+    "url": "/Portfolio/static/media/Phone-icon.09f577dd.png"
+  },
+  {
+    "revision": "82a318ccea22a8af9fc4590f41df9238",
+    "url": "/Portfolio/static/media/ReactLogo.82a318cc.png"
+  },
+  {
+    "revision": "400efb03d5ae4580f8cebf078f8c9547",
+    "url": "/Portfolio/static/media/adobe-indesign.400efb03.svg"
+  },
+  {
+    "revision": "4f9a447318540c537af4661b92b799e9",
+    "url": "/Portfolio/static/media/adobe-photoshop.4f9a4473.svg"
+  },
+  {
+    "revision": "87311a2965477700ef6354a04e9ae1e0",
+    "url": "/Portfolio/static/media/adobe-xd.87311a29.svg"
+  },
+  {
+    "revision": "efa137c944b75c82f331991e3cde88bb",
+    "url": "/Portfolio/static/media/airev.efa137c9.png"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/Portfolio/static/media/bootstrap.ffded238.svg"
+  },
+  {
+    "revision": "73ad816601f09413a1e72cddee60b328",
+    "url": "/Portfolio/static/media/business-campus.73ad8166.png"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/Portfolio/static/media/css.3707da3c.svg"
+  },
+  {
+    "revision": "422e766543a3850de8db1f049775a1e9",
+    "url": "/Portfolio/static/media/facebook.422e7665.svg"
+  },
+  {
+    "revision": "1e430b5d594a231808b46c68874237a6",
+    "url": "/Portfolio/static/media/fintalent.1e430b5d.png"
+  },
+  {
+    "revision": "7f8d489c1887554fcf61932505d46393",
+    "url": "/Portfolio/static/media/firebase.7f8d489c.svg"
+  },
+  {
+    "revision": "8f8edc5fb64b9bd436834dc926ac992c",
+    "url": "/Portfolio/static/media/github.8f8edc5f.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/Portfolio/static/media/html.676e8188.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/Portfolio/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "154629f25d417c8c0a4ac3a83707979a",
+    "url": "/Portfolio/static/media/linkedin.154629f2.svg"
+  },
+  {
+    "revision": "a3d48c4f69c70789cced108ce7c13c4c",
+    "url": "/Portfolio/static/media/malta-park.a3d48c4f.png"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/Portfolio/static/media/sass.b446ca5a.svg"
+  },
+  {
+    "revision": "06bcd6d305deb719d9bfedbad9f5451d",
+    "url": "/Portfolio/static/media/skills.06bcd6d3.svg"
+  },
+  {
+    "revision": "774436385f5a78a8598a805d8179bce0",
+    "url": "/Portfolio/static/media/styled-components.77443638.png"
+  },
+  {
+    "revision": "e60fcd0974402a2aaf59f3835776c5ea",
+    "url": "/Portfolio/static/media/visual-studio.e60fcd09.svg"
+  },
+  {
+    "revision": "dfda02906a18e021e14c2b94b77fe006",
+    "url": "/Portfolio/static/media/warsawJS.dfda0290.jpg"
+  },
+  {
+    "revision": "837b970d5c77507b2ea4bb0ec18059a0",
+    "url": "/Portfolio/static/media/webpack.837b970d.svg"
+  }
+]);
